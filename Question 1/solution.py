f080cec09a0bd8830757097ff9e6e6de18a0b7a0
mshleung <leungsiumat@gmail.com>
2026-02-11 13:32:03 +0800

    test2

---
+++ Question 1/solution.py
@@ -0,0 +1,8 @@
+def generate_pyramid(height):
+    """
+    Generate a pyramid pattern of the specified height.
+    
+    Args:
+        height: The number of rows in the pyramid
+    """
+    pass
